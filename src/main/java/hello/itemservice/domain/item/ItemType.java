3cf076ef9f3998f98ab0f43c32df05b8d47cede3
master <ted3131@diquest.com>
2024-01-17 14:30:56 +0900

    commit

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+
+    BOOK("도서") ,FOOD("음식") ,ETC("기타");
+
+    private final String discription;
+
+    ItemType(String discription) {
+        this.discription = discription;
+    }
+}
