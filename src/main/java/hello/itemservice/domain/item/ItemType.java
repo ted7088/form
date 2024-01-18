f8ea460e68b7c87d319e95c4bfa2dfddb5d1dfe9
master <ted3131@diquest.com>
2024-01-18 14:32:45 +0900

    commit

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -4,9 +4,14 @@ public enum ItemType {
 
     BOOK("도서") ,FOOD("음식") ,ETC("기타");
 
-    private final String discription;
+    private final String description;
 
-    ItemType(String discription) {
-        this.discription = discription;
+    ItemType(String description) {
+        this.description = description;
+    }
+
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -29,6 +30,11 @@ public class FormItemController {
         regions.put("JEJU", "제주");
 
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
     }
 
     @GetMapping
